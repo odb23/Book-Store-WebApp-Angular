c46edc95e287ad52b6e6bd13e3d5e3c83912b07e
odb23 <damijustice1@gmail.com>
2022-09-15 11:11:50 +0100

    added login and register from validations

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,31 @@
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  public createAccountForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.initializeForm();
   }
 
+  initializeForm() {
+    this.createAccountForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: [
+        '',
+        Validators.required,
+        Validators.pattern,
+      ],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required],
+    });
+  }
 }
